f0d736583f0ddb6fd0fd8de6880259ec90dbfaa3
Nibaldo Ruiz <nibaldo78@hotmail.com>
2023-04-18 18:00:54 -0400

    with your message here

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply); // Output: 62
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+console.log(mod); // Output: 4
+
+let setOfNumbers = [2, 5, 9, 12, 20, 4, 7];
+let max = Math.max(...setOfNumbers);
+
+console.log(max); // Output: 20
+
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random); // Output: a random integer between 1 and 100
+
